27d634e2f7131aa267f93ad8f8149bb7dde6662a
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-26 19:18:55 -0300

    up

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.tsx'
-import './index.css'
+import About from './pages/About.tsx'
+import Cards from './pages/Cards.tsx'
+import Home from './pages/Home.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<App />}>
+          <Route path='/' element={<Home />} />
+          <Route path='cards' element={<Cards />} />
+          <Route path='about' element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
