dcf9ed6a5a7232e4e1c5750234da5915de5b9328
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-27 13:02:27 -0300

    up

--- src/components/Topcards/Topcards.tsx
+++ src/components/Topcards/Topcards.tsx
@@ -2,6 +2,7 @@ import Mago from '../../images/magonegro.jpg'
 import Dragao from '../../images/dragaobranco.jpg'
 import Slifer from '../../images/slifer.png'
 import Dark from '../../images/Dark.webp'
+import Luz from '../../images/Luz.png'
 import { FaStar } from "react-icons/fa"
 
 import './Topcards.sass'
@@ -9,11 +10,11 @@ import './Topcards.sass'
 const Topcards = () => {
   return (
     <div className='topcards'>
-      <h1>Top cards</h1>
+      <h1 id='nome_top'>Top cards</h1>
       <div className='principal'>
         <div className='container'>
           <div className='nome_carta'>
-            <h1>Mago Negro</h1>
+            <h1 id='nome_mago'>Mago Negro</h1>
             <img src={Dark} alt="Icone Dark" />
           </div>
           <div className='stars'>
@@ -30,9 +31,10 @@ const Topcards = () => {
           </figure>
           <p>[MAGO] <br />O mago definitivo em termos de ataque e defesa. <br /> <br /> <br /><hr />ATK/2500 DEF/2100</p>
         </div>
-        <div className='container'>
+        <div className='container' id='slifer'>
           <div className='nome_carta'>
-            <h1>Slifer</h1>
+            <h1>Slifer, o Dragão Celeste</h1>
+            <img src={Luz} alt="Icone Luz" />
           </div>
           <div className='stars'>
             <div className='star'><FaStar  /></div>
@@ -49,11 +51,12 @@ const Topcards = () => {
           <figure>
               <img src={Slifer} alt="Slifer" />
           </figure>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non harum doloremque dignissimos tenetur dolorum eum quisquam sunt tempora vero molestias sit accusamus consequatur ipsa fuga ducimus accusantium, architecto ex ratione.</p>
+          <p>[BESTA DIVINA] <br />Os céus se turvam e os trovôes rugem, anunciando a vinda desta criatura antiga, e a alvorada do verdadeiro poder. <br /> <br /> <hr />ATK/X000 DEF/X000</p>
         </div>
         <div className='container'>
           <div className='nome_carta'>
-            <h1>Dragao Branco</h1>
+            <h1 id='nome_dragao'>Dragão Branco de Olhos Azuis</h1>
+            <img src={Luz} alt="Icone Luz" />
           </div>
           <div className='stars'>
             <div className='star'><FaStar  /></div>
@@ -68,7 +71,7 @@ const Topcards = () => {
           <figure>
               <img src={Dragao} alt="Dragao branco" />
           </figure>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non harum doloremque dignissimos tenetur dolorum eum quisquam sunt tempora vero molestias sit accusamus consequatur ipsa fuga ducimus accusantium, architecto ex ratione.</p>
+          <p>[DRAGÃO/NORMAL] <br />Este dragão lendário é uma poderosa máquina de destruição. Praticamente invencível, muito poucos enfrentaram esta magnífica criatura e viveram para contar a história. <hr />ATK/3000 DEF/2500</p>
         </div>
       </div>
     </div>
