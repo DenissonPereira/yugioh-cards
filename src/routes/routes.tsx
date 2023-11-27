cbae5985ca7ed6d0614b1e097e65e8c3ec92c501
Denisson Pereira Santos <denisson159@gmail.com>
2023-11-26 22:07:31 -0300

    up

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from 'react-router-dom';
-import Home from '../pages/Home';
-import Cards from '../pages/Cards';
+import Home from '../pages/Home/Home';
+import Cards from '../pages/Cards/Cards';
 
 
 function RoutesApp() {
