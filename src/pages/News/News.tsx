609f65b3efa1a9ef8d00e7f414dcb647ef0b3ddb
Denisson Pereira Santos <denisson159@gmail.com>
2023-12-02 08:59:38 -0300

    create news

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -3,6 +3,8 @@ import Footer from "../../components/layout/Footer/Footer"
 import Header from "../../components/layout/Header/Header"
 
 import Avo from "../../images/img/avo.jpg"
+import Deuses from '../../images/img/deuses.webp'
+import Mago from '../../images/img/mago_negro.jpg'
 
 import './News.sass'
 
@@ -17,23 +19,42 @@ const News = () => {
         <Header />
       </section>
       <section className="caixa_news">
-        <div>
+        <h1><span>&raquo; </span>Notícias <span>&laquo;</span></h1>
+        <div className="caixa_noticias">
           <div className="noticia_menor">
-            <h1>Conhecendo um pouco mais sobre Yugi Moto e sua história</h1>
-            <h2>Yugi Moto é o protagonista de Yu-Gi-Oh!, um anime e mangá. Sua história gira em torno de desafios em jogos de cartas, especialmente o Duelo de Monstros, enquanto ele descobre o poder de um antigo quebra-cabeça do milênio que altera seu destino.</h2>
-            <div className="noticia1">
-              <figure>
-                <img src={Avo} alt="Avo de Yugi" />
-              </figure>
-              <h1><strong>Conheça mais sobre o deck do avô de Yugi</strong></h1>
-              <h2>O avô de Yugi em Yu-Gi-Oh! é Solomon Muto, um experiente jogador de jogos de tabuleiro e dono da loja de jogos "Kame Game"</h2>
+            <div className="news_inicio">
+              <h1><span>&raquo; </span>Conhecendo um pouco mais sobre Yugi Moto e sua história <span>&laquo;</span></h1>
+              <h2>Yugi Moto é o protagonista de Yu-Gi-Oh!, um anime e mangá. Sua história gira em torno de desafios em jogos de cartas, especialmente o Duelo de Monstros, enquanto ele descobre o poder de um antigo quebra-cabeça do milênio que altera seu destino.</h2>
             </div>
-            <div className="noticia2">
-
+            <div className="linha_news">
+              <div className="noticia1">
+                <figure>
+                  <img src={Avo} alt="Avo de Yugi" />
+                </figure>
+                <h1><strong>Conheça mais sobre o deck do avô de Yugi</strong></h1>
+                <h2>O avô de Yugi em Yu-Gi-Oh! é Solomon Muto, um experiente jogador de jogos de tabuleiro e dono da loja de jogos "Kame Game"...</h2>
+                <button>LER MAIS</button>
+              </div>
+              <div className="noticia2">
+                <figure>
+                  <img src={Deuses} alt="Deuses" />
+                </figure>
+                <h1><strong>Conheça as cartas dos deuses egípcios</strong></h1>
+                <h2>
+                  As cartas dos deuses egípcios são elementos fundamentais do jogo de cartas Yu-Gi-Oh!, originário do Japão. Cada carta representa um deus ou ser mitológico do Antigo Egito, conferindo habilidades únicas aos jogadores durante as partidas...</h2>
+                <button>LER MAIS</button>
+              </div>
             </div>
           </div>
           <div className="noticia_maior">
-
+            <figure>
+              <img src={Mago} alt="Yugi e Mago Negro" />
+            </figure>
+            <div className="noticia_fig_maior">
+              <h1>O faraó egípcio, mago negro e Yugi formam uma conexão intrigante, desencadeando desafios épicos em Yu-Gi-Oh!</h1>
+              <h2>O Mago Negro é uma icônica carta no jogo de cartas Yu-Gi-Oh!, representando um feiticeiro poderoso vestido de negro. Conhecido por sua presença imponente e habilidades mágicas, o Mago Negro é frequentemente associado ao protagonista Yugi na série...</h2>
+              <button>LER MAIS</button>
+            </div>
           </div>
         </div>
       </section>
